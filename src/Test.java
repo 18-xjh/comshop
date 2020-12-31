057db5dd9ad466a5bb0f65c2f343763dc8f4be43
18-xjh <535309586@qq.com>
2020-12-31 16:15:32 +0800

    12月26号代码

--- src/Test.java
+++ src/Test.java
@@ -1,28 +1,30 @@
 import java.io.File;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Test {
+    private static User[] users=null;
+
     public static  void main(String[] args){
-        System.out.println("请输入用户名: ");
-        Scanner sc=new Scanner(System.in);
-        String username=sc.next();
-        System.out.println("刚刚输入的用户名: "+username);
-        System.out.println("请输入密码:");
-
-        String password=sc.next();
-        System.out.println("刚刚输入的密码: "+password);
-
-        /*
+                /*
         开始读文件
          */
-        File file=new File("C:\\Users\\dell\\IdeaProjects\\comshop\\users.xlsx");
+        File file=new File("C:\\Users\\dell\\IdeaProjects\\comshop\\src\\users.xlsx");
         ReadExcel readExcel=new ReadExcel();//创建对象
-        User users[]=readExcel.readExcel(file);
-        System.out.println(users.length);
-        for(User user:users){
-            System.out.print(user.getUsername());
-            System.out.println("\t"+user.getPassword());
-        }
+        User Users[]=readExcel.readExcel(file);
 
+        System.out.println("请输入用户名: ");
+
+        Scanner sc=new Scanner(System.in);
+        String username=sc.next();
+
+        System.out.println("请输入密码:");
+        String password=sc.next();
+
+        for(User user:users){
+            if(username.equals(user.getUsername())&&password.equals(user.getPassword())){
+                System.out.println("登录成功");
+            }
+        }
     }
 }
